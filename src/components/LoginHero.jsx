c2231ed3ccaaa6ca20940abff56c8d1b675fac1d
Bright Gee Varghese <brightlsom@gmail.com>
2023-10-03 11:38:45 -0500

    Update LoginHero.jsx

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -10,6 +10,9 @@ function Hero({handleLogIn}) {
       <Wrapper>
         <Card>
           <FlexColumnCenter>
+            <h1>The path to your next</h1>
+            <h2>Open Source</h2>
+            <h1>contribution and more pizza.</h1>
             <h1>Open Sauced Dashboard</h1>
             <p>Login with GitHub to start tracking open source projects.</p>
             <br />
